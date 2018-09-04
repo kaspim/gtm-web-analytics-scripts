95b0ad17a33beee53bd99fc2962d77587d296f1e
Martin Kašpar <hello@martinkaspar.net>
2018-09-04 18:27:58 +0200

    GTM Web Analytics JavaStripts

--- src/custom-javascript-variable/adblockingdetection.js
+++ src/custom-javascript-variable/adblockingdetection.js
@@ -10,7 +10,7 @@ function () {
 	
 	fetch(adRequest).then(function(r) {
 		return 'ad blocking is not active';
-	}).catch(function(e){
+	}).catch(function(e) {
 		return 'ad blocking is active';
 	});
 }
