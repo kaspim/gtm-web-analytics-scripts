5f1b28771105e4a36360862f43c20bebfe0958c1
Martin Kašpar <hello@martinkaspar.net>
2018-09-04 18:21:24 +0200

    GTM Web Analytics JavaStripts

---
+++ src/custom-javascript-variable/adblockingdetection.js
@@ -0,0 +1,16 @@
+/*
+ * Custom JavaScript variable for ad blocking detection
+ *
+ * Author: Martin Kaspar - www.martinkaspar.net
+ * GitHub: GTM Analytics Scripts - github.com/kaspim/gtm-web-analytics-scripts
+ */
+
+function () {
+	var adRequest = new Request('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', { method: 'HEAD', mode: 'no-cors' });
+	
+	fetch(adRequest).then(function(r) {
+		return 'ad blocking is not active';
+	}).catch(function(e){
+		return 'ad blocking is active';
+	});
+}
