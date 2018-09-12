e6d3bd6698f90a8cb18c0b8f29ff829d1134777b
Martin Kašpar <hello@martinkaspar.net>
2018-09-12 07:07:53 +0200

    GTM Web Analytics JavaStripts

--- src/javascript-tags/adblockingdetection.js
+++ src/javascript-tags/adblockingdetection.js
@@ -5,20 +5,20 @@
  * GitHub: GTM Web Analytics JavaStripts - github.com/kaspim/gtm-web-analytics-scripts
  */
 
-function adblockingdetect(callback) {
+function adblocking(callback) {
 	var adRequest = new Request('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', { method: 'HEAD', mode: 'no-cors' });
 	
 	fetch(adRequest).then(function(r) {
-		callback(true);
-	}).catch(function(e) {
 		callback(false);
+	}).catch(function(e) {
+		callback(true);
 	});
 }
 
-adblockingdetect(function(connect) {
+adblocking(function(connect) {
 	window.dataLayer = window.dataLayer || [];
 	dataLayer.push({
 		event: 'adblocking',
-		ads: (connect === true) ? 'ads are not blocked' : 'ads are blocked'
+		ads: (connect === false) ? 'ads are not blocked' : 'ads are blocked'
 	});
 });
